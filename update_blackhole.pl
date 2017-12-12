15e92962923dd3116a73e75eb587f9aebfb4f598
Nigel Horne <njh@bandsman.co.uk>
2017-12-12 15:27:19 -0500

    Remove disappeared sites

--- update_blackhole.pl
+++ update_blackhole.pl
@@ -1,13 +1,17 @@
 #!/usr/bin/perl -w
 
 use LWP::Simple;
+use LWP::Protocol::https;
 
-our @URLs = ( 'http://adaway.sufficientlysecure.org/hosts.txt',
+our @URLs = ( # 'http://adaway.sufficientlysecure.org/hosts.txt',
+	      # 'https://adaway.org/hosts.txt',
 	      'http://winhelp2002.mvps.org/hosts.txt',
-	      'http://hosts-file.net/ad_servers.asp',
+	      # 'http://hosts-file.net/ad_servers.asp',
+	      'http://hosts-file.net/ad_servers.txt',
 	      'http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext',
 	      'http://www.malwaredomainlist.com/hostslist/hosts.txt',
-	      'http://adblock.gjtech.net/?format=unix-hosts' );
+	      # 'http://adblock.gjtech.net/?format=unix-hosts'
+	      );
 
 our %done;
 our %domains;
